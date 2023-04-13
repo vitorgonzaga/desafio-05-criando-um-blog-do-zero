a8d06ac202f85de474a5f3d8af3f4eb14c42fc5f
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-04-13 16:48:18 -0300

    feat (Home): getting data from prismic API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
+// import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 interface Post {
@@ -24,13 +24,25 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  console.log(JSON.stringify(postsPagination, null, 2));
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+  return (
+    <main className={styles.contentContainer}>
+      <h1>Home</h1>
+    </main>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('post');
+
+  return {
+    props: {
+      postsPagination: postsResponse,
+    },
+  };
+
+  // TODO
+};
