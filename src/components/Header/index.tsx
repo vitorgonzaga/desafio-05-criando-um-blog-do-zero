fd3a84ae8be4d2a722014c7f07a4c1bb684d0819
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-04-13 15:58:59 -0300

    feat: add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/logo.svg" alt="logo" />
+      </div>
+    </header>
+  );
 }
