e600148aecbd47fb5a96bd68a0150d22d9a400b4
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-04-15 14:58:01 -0300

    feat: add post/[slug] component

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,8 +1,9 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
-
+import { RichText } from 'prismic-dom';
+import BasicInfo from '../../components/BasicInfo';
 import { getPrismicClient } from '../../services/prismic';
 
-import commonStyles from '../../styles/common.module.scss';
+// import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
 interface Post {
@@ -26,20 +27,80 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps): JSX.Element {
+  console.log('Post -> post:', post);
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+  const words = post.data.content.reduce((acc, curr) => {
+    const headingWords = curr?.heading.split(' ').length;
+    const bodyWords = RichText.asText(curr?.body).split(' ').length;
+    return acc + headingWords + bodyWords;
+  }, 0);
 
-//   // TODO
-// };
+  return (
+    <main className={styles.contentContainer}>
+      <article className={styles.post}>
+        <img src={`${post.data.banner.url}`} alt="banner" />
+        <h1>{post.data.title}</h1>
+        <BasicInfo
+          publicationDate={post.first_publication_date}
+          author={post.data.author}
+          minutes={words}
+        />
+        <div
+          className={styles.postContent}
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: RichText.asHtml(post.data.content[0].body),
+          }}
+        />
+      </article>
+    </main>
+  );
+}
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient({});
+  const posts = await prismic.getByType('post');
 
-//   // TODO
-// };
+  return {
+    paths: [
+      { params: { slug: posts.results[0].uid } },
+      { params: { slug: posts.results[1].uid } },
+      { params: { slug: posts.results[2].uid } },
+    ],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const prismic = getPrismicClient({});
+  const { slug } = params;
+  const response = await prismic.getByUID('post', String(slug), {});
+  // console.log(
+  //   'constgetStaticProps:GetStaticProps= -> response:',
+  //   JSON.stringify(response, null, 2)
+  // );
+
+  const post = {
+    first_publication_date: response.first_publication_date,
+    data: {
+      title: response.data.title,
+      banner: {
+        url: response.data.banner.url,
+      },
+      author: response.data.author,
+      content: [
+        {
+          heading: response.data.content[0].heading,
+          body: response.data.content[0].body,
+        },
+      ],
+    },
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
