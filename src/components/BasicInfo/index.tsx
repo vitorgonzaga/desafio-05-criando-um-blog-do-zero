3e20a24e67e13068e924c2bf4543f3a1f491f877
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-04-15 14:27:32 -0300

    feat: create BasicInfo component

---
+++ src/components/BasicInfo/index.tsx
@@ -0,0 +1,60 @@
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+
+import styles from './basicinfo.module.scss';
+
+interface BasicInfoProps {
+  publicationDate: string;
+  author: string;
+  minutes?: number;
+}
+
+export default function BasicInfo({
+  publicationDate,
+  author,
+  minutes,
+}: BasicInfoProps): JSX.Element {
+  //   const [time, setTime] = useState(0);
+
+  //   const calculateTime = (): number => {
+  //     if (minutes > 60) {
+  //       return minutes / 60;
+  //     }
+  //     if (minutes === 60) {
+  //       return 1;
+  //     }
+  //     return minutes;
+  //   };
+
+  //   useEffect(() => {
+  //     setTime(calculateTime());
+  //   }, [minutes]);
+
+  return (
+    <>
+      <div className={styles.info}>
+        <div>
+          <FiCalendar />
+          <time>
+            {format(new Date(publicationDate), 'dd MMM yyyy', {
+              locale: ptBR,
+            })}
+          </time>
+        </div>
+        <div>
+          <FiUser />
+          <span>{author}</span>
+        </div>
+        {minutes ? (
+          <div>
+            <FiClock />
+            <span>{minutes}</span>
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
+    </>
+  );
+}
