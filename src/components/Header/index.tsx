4a6c6b0e8bf049c2794b932ea8e5234b34dde24a
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-04-15 10:30:07 -0300

    refactor (Header): wrapping the img tab with Link component from 'next/link'

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/logo.svg" alt="logo" />
+        <Link href="/">
+          <img src="/logo.svg" alt="logo" />
+        </Link>
       </div>
     </header>
   );
